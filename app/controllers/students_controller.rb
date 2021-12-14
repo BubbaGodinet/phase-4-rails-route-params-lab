340ebe6bf88012786f7680cef3452c79850c01ba
Bubba Godinet <godinetvn@gmail.com>
2021-12-13 17:11:22 -0700

    commit tree bdd022fed063ba706f1b0b042bc73b6374ee9568

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,19 @@
 class StudentsController < ApplicationController
 
   def index
+    if params == |key1: "value1", key2: "value2"|  {
+    studentsNamed = Student.where(params[first_name: :first_name, last_name: :last_name])
+    render json: studentsNamed
+    else {
     students = Student.all
     render json: students
+    }
+  }
   end
 
-end
+  def show
+    student = Student.find(params[:id])
+    render json: student
+  end
+
+
